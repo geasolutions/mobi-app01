3768ecb8876673d9e52d86079b89e6302445217d
geasolutions <gea.solutions.mx@gmail.com>
2017-11-11 00:17:02 -0600

    modificando sitio destinoa http://innova-cuerna.byethost12.com

--- www/app/settings.js
+++ www/app/settings.js
@@ -15,7 +15,7 @@ Drupal.settings.debug = true;
 /* DRUPAL PATHS */
  
 // Site Path (do not use a trailing slash)
-Drupal.settings.site_path = 'https://dev-in-your-zone.pantheonsite.io'; // e.g. http://www.example.com
+Drupal.settings.site_path = 'http://innova-cuerna.byethost12.com'; // e.g. http://www.example.com
 
 // Default Services Endpoint Path
 Drupal.settings.endpoint = 'drupalgap';
